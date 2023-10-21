989f361f118daa8568d8176e0a76063c4107ef97
Mohammed-Abrar-Raihan-Chowdhury <abrar.chowdhury.232@northsouth.edu>
2023-10-21 20:06:47 +0600

    Third update to splash screen
    Comments are added

--- splash.h
+++ splash.h
@@ -2,11 +2,11 @@
 #define SPLASH_H
 #include <dos.h>
 
-void splash_screen(void);
-void print_1(void);
+void splash_screen(void); //The full splash screen function to use in the main program
+void print_1(void); //Functions to print the word "Royal Bank" in three separate parts
 void print_2(void);
 void print_3(void);
-void create_bank(void);
+void create_bank(void); //Creates the image of the bank
 
 
 void splash_screen(void)
@@ -16,25 +16,25 @@ void splash_screen(void)
     print_3();
     create_bank();
 
-    system("cls");
+    system("cls"); //Clears the screen
 }
 
 
-void print_1(void)
+void print_1(void) //First part of "Royal Bank"
 {
   printf(".______      ______   ____    ____  ___       __         .______        ___      .__   __.  __  ___  \n");
   printf("|   _  \\    /  __  \\  \\   \\  /   / /   \\     |  |        |   _  \\      /   \\     |  \\ |  | |  |/  /  \n");
   sleep(1);
 }
 
-void print_2(void)
+void print_2(void) //Second part of "Royal Bank"
 {
   printf("|  |_)  |  |  |  |  |  \\   \\/   / /  ^  \\    |  |        |  |_)  |    /  ^  \\    |   \\|  | |  '  /   \n");
   printf("|      /   |  |  |  |   \\_    _/ /  /_\\  \\   |  |        |   _  <    /  /_\\  \\   |  . `  | |    <    \n");
   sleep(1);
 }
 
-void print_3(void)
+void print_3(void) //Third part of "Royal Bank"
 {
   printf("|  |\\  \\   |  `--'  |     |  |  /  _____  \\  |  `----.   |  |_)  |  /  _____  \\  |  |\\   | |  .  \\   \n");
   printf("| _| `._|   \\______/      |__| /__/     \\__\\ |_______|   |______/  /__/     \\__\\ |__| \\__| |__|\\__\\  \n\n");
@@ -44,17 +44,17 @@ void print_3(void)
 void create_bank(void)
 {
     FILE *file;
-    char sentence[100];
+    char sentence[100]; //An array to store strings while printing
 
-    file = fopen("Royal Bank Logo.txt", "r");
+    file = fopen("Royal Bank Logo.txt", "r"); //Opens the required file
 
-    if (file == NULL)
+    if (file == NULL) //No file detected
     {
         printf("Error opening file");
     }
     else
     {
-        while (fgets(sentence, 100, file))
+        while (fgets(sentence, 100, file)) //Keeps running until the file is empty and out of strings
         {
             printf("%s", sentence);
         }
@@ -62,7 +62,6 @@ void create_bank(void)
 
     fclose(file);
     sleep(2);
-
 }
 
 #endif // SPLASH_H
