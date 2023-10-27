e6b9d6ed25ad76091cada6ab27313ebeb9bccb80
Mohammed-Abrar-Raihan-Chowdhury <abrar.chowdhury.232@northsouth.edu>
2023-10-27 16:55:24 +0600

    First update to menu.h
    Added comments

--- menu.h
+++ menu.h
@@ -1,13 +1,13 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
 
-void menu_process(int account_number);
-int actions(void);
+void menu_process(int account_number); //The entire menu process will be done with this function
+int actions(void); //Displays the actions the program can do and takes user input
 
-int verify_pin(int account_number);
-void update_structure(int account_number);
+int verify_pin(int account_number); //Verifies the users pin number
+void update_structure(int account_number); //Updates the structure with the users information
 
-struct credentials
+struct credentials //Structure for storing the users information
 {
     int pin;
     int savings;
@@ -17,12 +17,12 @@ struct credentials
 
 void menu_process(int account_number)
 {
-    int input = 0;
+    int input = 0; //For user input
     update_structure(account_number);
 
-    while (input != 4)
+    while (input != 4) //Will keep running until user quits
     {
-        input = actions();
+        input = actions(); //Stores user action input
 
         if (input == 1)
         {
@@ -38,9 +38,9 @@ void menu_process(int account_number)
         }
         else if (input == 4)
         {
-            printf("Goodbye! \n");
+            printf("Goodbye! \n"); //Quits program afterwards
         }
-        else
+        else //Invalid input message
         {
             printf("Incorrect input. Please try again \n");
         }
@@ -51,9 +51,9 @@ void menu_process(int account_number)
 
 int actions(void)
 {
-    int temp;
+    int temp; //For storing user input
 
-    printf("Please pick one of the following options \n");
+    printf("Please pick one of the following options \n"); //Displays all the things the program can do
     printf("1. Check Balance \n");
     printf("2. Deposit cash \n");
     printf("3. Withdraw cash \n");
@@ -61,43 +61,43 @@ int actions(void)
 
     printf("Enter your choice: ");
     scanf("%d", &temp);
-    return temp;
+    return temp; //Returns user input
 
 }
 
 void update_structure(int account_number)
 {
-    FILE *file_struct1, *file_struct2, *file_struct3;
+    FILE *file_struct1, *file_struct2, *file_struct3; //Files for updating structure values
 
-    file_struct1 = fopen("Pins.txt","r");
+    file_struct1 = fopen("Pins.txt","r"); //Opens all the files
     file_struct2 = fopen("Savings Accounts.txt","r");
     file_struct3 = fopen("Current Accounts.txt","r");
 
 
-    for (int i =0; i < account_number; i++)
+    for (int i =0; i < account_number; i++) //Locates the information of the account
     {
-        fscanf(file_struct1, "%d", &p1.pin);
+        fscanf(file_struct1, "%d", &p1.pin); //Store the information of the account
         fscanf(file_struct2, "%d", &p1.savings);
         fscanf(file_struct3, "%d", &p1.current);
     }
 
-    fclose(file_struct1);
+    fclose(file_struct1); // Closes all the files
     fclose(file_struct2);
     fclose(file_struct3);
 }
 
 int verify_pin(int account_number)
 {
-    int temp_pin = 0;
+    int temp_pin = 0; //For storing pin number
 
     printf("Enter your 4 digit pin: ");
     scanf("%d",&temp_pin);
 
-    if (temp_pin == p1.pin)
+    if (temp_pin == p1.pin) //Checks pin
     {
         return 1;
     }
-    else
+    else //Invalid pin
     {
         printf("Invalid pin detected \n");
         return 0;
