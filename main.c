0cc1d2f4e9ff7ce4d39613e8db7488a65e3c90ba
Mohammed-Abrar-Raihan-Chowdhury <abrar.chowdhury.232@northsouth.edu>
2023-11-04 19:20:14 +0600

    Updated main.c
    Added comments and small improvements

--- main.c
+++ main.c
@@ -5,19 +5,20 @@
 
 int main(void)
 {
-    int account_number;
+    int account_number; //For storing account number
 
     splash_screen();
-    account_number = login_process();
+    account_number = login_process(); //Stores the account number
 
     if (account_number)
     {
-        menu_process(account_number);
+        menu_process(account_number); //Uses account number in the menu process
     }
     else
     {
-        printf("File not detected");
+        printf("Files not detected"); //Error message
     }
 
     return 0;
 }
+
